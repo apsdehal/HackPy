dc31f1968868adda8095ecd7f8c91757d11563dc
apsdehal <apsdehal@gmail.com>
2014-01-28 02:19:23 +0530

    Completed TCP_Port_scanner
    Added threading to use multiple port at a single time, also used Semaphore class to acquire and release screenlock at the time of printing a message to avoid any misconfusion due to threading

--- TCP Port Connect/TCP_Port_Scanner.py
+++ TCP Port Connect/TCP_Port_Scanner.py
@@ -1,6 +1,8 @@
 import optparse
-import socket
 from socket import *
+from threading import *
+
+screenLock = Semaphore( value = 1 )
 
 def connScan( tgtHost, tgtPort ):
 	try:
@@ -8,11 +10,15 @@ def connScan( tgtHost, tgtPort ):
 		connSkt.connect( ( tgtHost, tgtPort ) )
 		connSkt.send( 'ViolentPython\r\n' )
 		results = connSkt.recv( 100 )
+		screenLock.acquire()
 		print '[+]%d/tcp open'% tgtPort
 		print '[+] ' + str(results)
-		connSkt.close()
 	except:
+		screenLock.acquire()
 		print '[-]%d/tcp closed'%tgtPort
+	finally:
+		screenLock.release()
+		connSkt.close()
 
 def portScan( tgtHost, tgtPorts ):
 	try:
@@ -27,17 +33,17 @@ def portScan( tgtHost, tgtPorts ):
 		print '\n[+] Scan Results for:' + tgtIP
 	setdefaulttimeout(1)
 	for tgtPort in tgtPorts:
-		tgtPort = tgtPort
+		t = Thread( target = connScan, args = ( tgtHost, int( tgtPort ) ) )
 		print 'Scanning Port ' + tgtPort
-		connScan( tgtHost, int( tgtPort ) )
+		t.start()
 
 def main():
 	parser = optparse.OptionParser( "usage%prog"+ "-H <target host> -p <target port>")
 	parser.add_option('-H',dest='tgtHost',type="string", help="Specify a target host")
-	parser.add_option('-p',dest='tgtPort',type="string", help="Specify target ports seperated by commas")
+	parser.add_option('-p',dest='tgtPort',type="string", help="Specify target ports seperated by commas E.g. 21,22,23")
 	(options, args) = parser.parse_args()
 	tgtHost = options.tgtHost
-	tgtPorts = str( options.tgtPort ).split(', ')
+	tgtPorts = str( options.tgtPort ).split(',')
 	if( tgtHost == None ) | (tgtPorts[0] == None):
 		print '[-] You must specify a target host and target port(s)'
 		exit(0)
