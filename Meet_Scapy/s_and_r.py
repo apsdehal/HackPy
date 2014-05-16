46e247319fb88c280f9531c42aa80d5e4eaef7c6
Amanpreet Singh <apsdehal@gmail.com>
2014-05-16 21:25:33 +0530

    Adds comment

--- Meet_Scapy/s_and_r.py
+++ Meet_Scapy/s_and_r.py
@@ -1,12 +1,21 @@
 #! /usr/bin/python
 import sys
 from scapy.all import *
-
+'''
+Takes first argument given at commandline
+'''
 dest = sys.argv[1]
 
+'''
+sr1 takes a packet sends it and returns first answered packet back
+dest: Destination to which packet has to be sent
+'''
 def sr1Example(dest):
 	return sr1(IP(dst=dest)/ICMP()/"Hello World")
 
+'''
+Print out the result of answered packed object
+'''
 def showResult(returnPacket):
 	returnPacket.show()
 
