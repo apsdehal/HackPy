c0bc8ae78412765ee1096c30dd9df542b216108f
Amanpreet Singh <apsdehal@gmail.com>
2014-05-16 20:49:54 +0530

    Adds more options to make this more viable

--- Meet_Scapy/first_face.py
+++ Meet_Scapy/first_face.py
@@ -3,8 +3,13 @@ from scapy.all import *
 
 '''
 Send an IMCP packet to google's server
+Use wireshark to capture packets and see whats really happening
 '''
-def send_packet():
-	send(IP(dst="8.8.8.8")/ICMP()/"Hello World");
-
-send_packet();	
+def send_packet(source,life = 64):
+	send(IP(src=source,dst="8.8.8.8",ttl=life)/ICMP()/"Hello World");
+'''
+You can manipulate the source by the src parameter in IP layer
+But if do so you won't get ping reply since you spoofed the source IP
+ttl: You can add this param to change the time to live for the packet, default: 64
+'''
+send_packet('10.0.0.1');	
