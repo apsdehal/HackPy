2cb2ce74705da60dd51e1392f1c8a3c13dc287b5
Amanpreet Singh <apsdehal@gmail.com>
2014-05-22 12:07:28 +0530

    Adds example for flag

--- Meet_Scapy/s_and_r.py
+++ Meet_Scapy/s_and_r.py
@@ -4,6 +4,9 @@ import sys
 dest = sys.argv[1]
 dport = sys.argv[2]
 
+'''
+You can also pass an array to dport so it will act like a port scanner (nmap)
+'''
 def srExample(dest, Dport):
 	return sr(IP(dst=dest)/TCP(dport=Dport)/"Hello World")
 
@@ -11,6 +14,14 @@ def showResult(packet):
 	ans, unans = packet
 	ans.summary()
 	unans.summary()
+'''
+You can also define the type of packets to be send using flag parameter, for e.g. for sending a SYN 
+packet send flag='S', for ACK send 'A', remember you need to do SYN ACK before you send ACK packets
+'''
+
+def srExampleWithFlag(dest, SPort, DPort, flag):
+	return sr(IP(dst=dest)/TCP(sport= SPort, dport= DPort, flags= flag)/'Hello World') #Sport is the sending port from your side
+
 
 p = srExample(dest, int(dport))
 showResult(p) 	 	
